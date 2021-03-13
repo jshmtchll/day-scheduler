20ba1bc215c3a7b289c2020a4d07f8a5997621ed
josh mitchell <jshmtchll@gmail.com>
2021-03-13 12:56:25 -0600

    changed html and added table and horizontal rule

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,2 +1,40 @@
+//displays the current date/time in <header>
 let time = moment();
-$(".today").text(moment().format("dddd, MMMM Do YYYY"));
+$("#today").text(moment().format("dddd, MMMM Do YYYY"));
+$('#currenttime').text(moment().format('h:mm a'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//hr animation
+jQuery(document).ready(function($){
+    setTimeout(function(){
+        $('.trans--grow').addClass('grow');
+    }, 275);
+});
